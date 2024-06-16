dfc76cdf6e54fb0044d77280bf01f684367ba0f5
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-17 00:40:05 +0700

    feat: add tanstack query, toaster

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,31 @@
 'use client'
 
 import { ThemeProvider } from '@/components/theme-providers'
-import React from 'react'
-
+import React, { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from '@/components/ui/toaster'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000 * 5,
+          },
+        },
+      }),
+  )
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <Toaster />
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
